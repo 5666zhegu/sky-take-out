f4527ed7cdadb130bfb7702ba2952d85dd7553af
Knight <liujunfeng242@gmail.com>
2026-02-05 18:01:27 +0800

    微信登陆相关代码开发

--- sky-server/src/main/java/com/sky/controller/user/ShopController.java
+++ sky-server/src/main/java/com/sky/controller/user/ShopController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ShopController {
     private static final String KEY = "SHOP_STATUS";
     @Autowired
+    
     private RedisTemplate redisTemplate;
 
     @GetMapping("/status")
