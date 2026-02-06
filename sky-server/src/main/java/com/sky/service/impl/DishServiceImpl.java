1481edd8e6f9fa7bed7c5952437ec87cbda0defb
Knight <liujunfeng242@gmail.com>
2026-02-06 14:05:40 +0800

    菜品更新代码修改

--- sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
@@ -22,6 +22,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -70,7 +71,7 @@ public class DishServiceImpl implements DishService {
 
         long total = page.getTotal();
 
-        List records = page.getResult();
+        List<DishVO> records = page.getResult();
 
 
         return new PageResult(total, records);
@@ -133,14 +134,15 @@ public class DishServiceImpl implements DishService {
 
         Long id = dishDTO.getId();
 
-        Long id1 = dish.getId();
-        dishFlavorMapper.deleteByDishId(id1);
-
+        dishFlavorMapper.deleteByDishId(id);
         List<DishFlavor> flavors = dishDTO.getFlavors();
-        flavors.forEach(flavor -> {
-            flavor.setId(dish.getId());
-        });
-        dishFlavorMapper.insertBatch(flavors);
+        if(flavors != null && !flavors.isEmpty()){
+
+            flavors.forEach(flavor -> {
+                flavor.setId(dishDTO.getId());
+            });
+            dishFlavorMapper.insertBatch(flavors);
+        }
 
     }
 
@@ -184,4 +186,29 @@ public class DishServiceImpl implements DishService {
                 .build();
         dishMapper.startOrStop(dish);
     }
+
+    /**
+     * 条件查询菜品和口味
+     * @param dish
+     * @return
+     */
+    public List<DishVO> listWithFlavor(Dish dish) {
+        List<Dish> dishList = dishMapper.list(dish);
+
+        List<DishVO> dishVOList = new ArrayList<>();
+
+        for (Dish d : dishList) {
+            DishVO dishVO = new DishVO();
+            BeanUtils.copyProperties(d,dishVO);
+
+            //根据菜品id查询对应的口味
+            List<DishFlavor> flavors = dishFlavorMapper.getByDishId(d.getId());
+
+            dishVO.setFlavors(flavors);
+            dishVOList.add(dishVO);
+        }
+
+        return dishVOList;
+    }
+
 }
